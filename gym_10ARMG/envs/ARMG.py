5bf4f7ce2bfa25cde069a65538b116c0e1b494fd
Areeb <87232965+Arieb-007@users.noreply.github.com>
2021-09-09 09:32:44 +0530

    Create ARMG.py

---
+++ gym_10ARMG/envs/ARMG.py
@@ -0,0 +1,28 @@
+from gym import spaces
+import numpy as np
+from gym.utils import seeding
+import gym
+
+class Ten_ARMG:
+    
+    def __init__(self,sigma,bandits):
+        self.sigma = sigma               #standard deviation
+        self.bandits = bandits
+        
+    
+    def prob_dist(self,no_samples_per_arm,bandit):
+        p_dist = []         
+        for n in range(no_samples_per_arm):
+            seed_ = n + bandit
+            self.seed(seed_)
+            p_dist.append(np.random.normal(0,self.sigma))
+        return p_dist
+    
+    def rew_dist(self,q_mean,seed):
+        rew_dist = []
+        np.random.seed(seed)
+        return np.random.normal(q_mean,self.sigma)
+        
+            
+    def seed(self,seed):
+        np.random.seed(seed)
